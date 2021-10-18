1b484c52c4b7954c80525cb37bc318085ec0395b
Vijay Shiyani <vshiyani@meeco.me>
2021-10-19 10:52:18 +1100

    add swagger ui support

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Sample assignment')
+    .setDescription('The sample assignment API description')
+    .setVersion('1.0')
+    .addTag('assignment')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
